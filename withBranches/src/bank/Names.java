d364a2e03077b46a6e0f0230534651f28afd2ed2
Gosha Krikun <gosha@krikun.org>
2015-07-24 01:56:37 +0400

    seems fine

---
+++ withBranches/src/bank/Names.java
@@ -0,0 +1,22 @@
+package bank;
+
+import java.util.Random;
+
+/**
+ * Created by krikun on 24.07.2015.
+ */
+public class Names {
+    private static String[] firstNames = {"Ulrike", "Raymond", "Andria", "Euna", "Marvin", "Debrah", "Mirian", "Jamaal", "Crystal", "Irene", "Quinton", "Wilton", "Jeanice", "Ira", "Deadra", "Layla", "Timothy", "Blondell", "Kali", "Adrianna", "Mollie", "Neil", "Aline", "Santos", "Shameka", "Marcene", "Homer", "Pete", "Eun", "Ruben"};
+
+    private static String[] lastNames = {"Mendoza", "Horton", "Cherry", "Banks", "Osborn", "Schmitt", "Mullen", "Sweeney", "Buchanan", "Mclaughlin", "Ballard", "Winters", "Haley", "Patton", "Andersen", "Daniel", "Henry", "Chang", "Villegas", "Allison", "Mckinney", "Ramirez", "Cline", "Harmon", "Salazar", "Vasquez", "Haas", "Rocha", "Moss", "Kidd"};
+
+    public static String getRandomFirstName() {
+        Random random = new Random();
+        return firstNames[random.nextInt(firstNames.length)];
+    }
+
+    public static String getRandomLastName() {
+        Random random = new Random();
+        return lastNames[random.nextInt(lastNames.length)];
+    }
+}
---
+++ withBranches/src/bank/Transactions.java
@@ -0,0 +1,33 @@
+package bank;
+
+/**
+ * Created by krikun on 23.07.2015.
+ */
+public interface Transactions {
+
+    /**
+     * get money from account
+     * @param sum
+     */
+    double withdraw(Customer customer, double sum);
+
+    /**
+     * put money to the account
+     * @param sum
+     */
+    double deposit(Customer customer, double sum);
+
+    /**
+     * add percents of balance
+     */
+    double payInterests(Customer customer);
+
+    /**
+     * transfer money from transmitter to receiver
+     * @param transmitter
+     * @param receiver
+     * @param sum
+     */
+    double transfer(Customer transmitter, Customer receiver, double sum);
+
+}
